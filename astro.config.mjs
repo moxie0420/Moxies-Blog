6311f4f4acc3c6ddb4f26361b31d971aa9b5e4f7
Moxie Benavides <moxie@moxiege.com>
2024-06-26 03:52:13 +0000

    remove node_modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +12,6 @@ export default defineConfig({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  integrations: [react(), mdx(), tailwind()]
 });
