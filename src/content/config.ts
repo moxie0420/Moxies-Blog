0e6f51900c3139c2698acfb74d50da45c70b1c70
Moxie Benavides <moxie@moxiege.com>
2024-06-30 17:23:42 +0000

    add subtitle to frontmater start next post fixt navigation to blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,19 @@
 import { z, defineCollection } from "astro:content";
 
+const image = z.object({
+  url: z.string(),
+  alt: z.string(),
+});
+
 const postCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    subtitle: z.string(),
     description: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    worksImage1: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    worksImage2: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    image: image,
+    worksImage1: image,
+    worksImage2: image,
     platform: z.string(),
     stack: z.string(),
     website: z.string(),
